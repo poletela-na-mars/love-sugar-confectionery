f6ffbe3a96478ae6fa8a90ad7fa9e6c23ed299b3
Polina <polina-postylyakova@yandex.ru>
2023-07-28 00:16:58 +0300

    saving the filtering parameters in the URL

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,34 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
 
-import { FilterState, setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { SearchContext } from '../App';
+import { FilterState, setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 
 import { Categories, ItemBlock, Pagination, Sort } from '../components';
 import { Skeleton } from '../components/ItemBlock/Skeleton';
 
-import { SearchContext } from '../App';
+import { sortList } from '../consts';
 
 export const Home = () => {
-  const selectState = (state: FilterState) => state.filterSlice;
-  const { categoryId, sort, currentPage } = useSelector(selectState);
-
-  const dispatch = useDispatch();
-
-  const onClickCategoryHandler = (id: number) => {
-    dispatch(setCategoryId(id));
-  };
-
-  const { searchValue } = useContext(SearchContext);
-
   const [products, setProducts] = useState<any>([]);
   // TODO - set correct type for product
   //      - fix a circle for amount of product
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  const selectState = (state: FilterState) => state.filterSlice;
+  const { categoryId, sort, currentPage } = useSelector(selectState);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isSearch = useRef<boolean>(false);
+  const isMounted = useRef<boolean>(false);
+
+  const { searchValue } = useContext(SearchContext);
+
+  const fetchProducts = () => {
     setIsLoading(true);
 
     const sortBy = sort.sortProperty.replace('-', '');
@@ -40,13 +42,53 @@ export const Home = () => {
           setProducts(res.data);
           setIsLoading(false);
         })
+  };
 
-    window.scrollTo(0, 0);
-  }, [categoryId, sort, searchValue, currentPage]);
+  const onClickCategoryHandler = (id: number) => {
+    dispatch(setCategoryId(id));
+  };
 
   const onPageChangeHandler = (number: number) => {
     dispatch(setCurrentPage(number));
   };
+
+  useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+
+      const sort = sortList.find((obj) => obj.sortProperty === params.sortProperty);
+
+      dispatch(setFilters({
+        ...params,
+        sort
+      }));
+      isSearch.current = true;
+    }
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    if (!isSearch.current) {
+      fetchProducts();
+    }
+
+    isSearch.current = false;
+  }, [categoryId, sort.sortProperty, currentPage]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      });
+
+      navigate(`?${queryString}`);
+    }
+
+    isMounted.current = true;
+  }, [categoryId, sort.sortProperty, currentPage]);
 
   const mappedProducts = products
       .map((product: { id: any; }) => <ItemBlock key={product.id} {...product} />);
